1c5095cc5e01d162ecc1631606750c9bfc71db21
donghualei <donghualei@baidu.com>
2017-10-23 21:43:36 +0800

    基础文件

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @file search-ui parser plugin for fis3
+ * @author donghualei <donghualei@baidu.com>
+ @ author cxtom
+ */
+
+const atom = require('vip-server-renderer');
+
+module.exports = function (content, file, options = {}) {
+
+};
